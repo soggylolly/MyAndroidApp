9e50f3a0d5e111aa42389e3e0ed847b1bba72538
soggylolly <thomas.kindon@yahoo.com>
2026-03-07 14:12:15 +0000

    completed worksheet 2 button date picker time picker

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -8,6 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
+import android.view.View;
+
+import android.widget.EditText;
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+
+import android.app.TimePickerDialog;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +30,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void onSaveClick(View view) {
+
+        Log.d("ToDoApp","Save button clicked");
+
+    }
+
+    public void openDatePicker(View view){
+
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                this,
+                (datePicker, y, m, d) -> {
+
+                    EditText dateInput = findViewById(R.id.taskDueDateView);
+                    dateInput.setText(d + "/" + (m+1) + "/" + y);
+
+                },
+                year, month, day
+        );
+
+        dialog.show();
+    }
+    public void openTimePicker(View view) {
+
+        Calendar calendar = Calendar.getInstance();
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog dialog = new TimePickerDialog(
+                this,
+                (timePicker, h, m) -> {
+                    EditText timeInput = findViewById(R.id.taskDueTimeView);
+                    timeInput.setText(h + ":" + m);
+                },
+                hour, minute, true
+        );
+
+        dialog.show();
     }
 }
